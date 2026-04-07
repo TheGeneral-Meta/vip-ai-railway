d9c144b18f96bb19d03bc374afa27c30bbb2d200
TheGeneral-Meta <vipstafsus@gmail.com>
2026-04-08 01:11:13 +0700

    Create next-auth.d.ts

---
+++ types/next-auth.d.ts
@@ -0,0 +1,34 @@
+import "next-auth"
+
+declare module "next-auth" {
+  interface User {
+    id: string
+    email: string
+    name: string
+    quota: number
+    plan: string
+    created_at?: string
+  }
+  
+  interface Session {
+    user: {
+      id: string
+      email: string
+      name: string
+      quota: number
+      plan: string
+      created_at?: string
+    }
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string
+    email: string
+    name: string
+    quota: number
+    plan: string
+    created_at?: string
+  }
+}
